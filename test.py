bbe549d45232947eae8b4b09d58f95a458b3986c
RattaPhong2004 <madpain533@gmail.com>
2024-12-07 20:48:18 +0700

    Update Function main

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 from datetime import datetime
 import pytz
+
+def main():
+    while True:
+        print("\nChoose a region:")
+        print("1. Asia\n2. Europe\n3. America\n4. Exit")
+        region_choice = input("Select an option (1-4): ")
